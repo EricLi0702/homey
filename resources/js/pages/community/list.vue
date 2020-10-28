2399ec4fc74797429fc9534124a5b75516dbe005
Pavel coruchakin <superpromiser@gmail.com>
2020-10-28 13:53:59 +0800

    community

--- resources/js/pages/community/list.vue
+++ resources/js/pages/community/list.vue
@@ -14,69 +14,69 @@
                         </div>
                         <div class="community-category-list p-3">
                             <div class=" ccl-item d-flex justify-content-between">
-                                <p>something</p>
-                                <Badge type="primary" :count="10"></Badge>
+                                <p>(1)	What comes to mind when you hear the word ‘rent’?</p>
+                                <Badge type="primary" :count="64"></Badge>
                             </div>
                             <div class=" ccl-item d-flex justify-content-between">
-                                <p>something</p>
-                                <Badge type="primary" :count="10"></Badge>
+                                <p>(2)	Do you think landlords charge too much for rent?</p>
+                                <Badge type="primary" :count="37"></Badge>
                             </div>
                             <div class=" ccl-item d-flex justify-content-between">
-                                <p>something</p>
-                                <Badge type="primary" :count="10"></Badge>
+                                <p>(3)	Is it better to rent or buy property?</p>
+                                <Badge type="primary" :count="15"></Badge>
                             </div>
                             <div class=" ccl-item d-flex justify-content-between">
-                                <p>something</p>
-                                <Badge type="primary" :count="10"></Badge>
+                                <p>(4)	Should kids with jobs pay rent to their parents?</p>
+                                <Badge type="primary" :count="48"></Badge>
                             </div>
                             <div class=" ccl-item d-flex justify-content-between">
-                                <p>something</p>
-                                <Badge type="primary" :count="10"></Badge>
+                                <p>(5)	What do you need to think about before renting a place to live?</p>
+                                <Badge type="primary" :count="12"></Badge>
                             </div>
                             <div class=" ccl-item d-flex justify-content-between">
-                                <p>something</p>
-                                <Badge type="primary" :count="10"></Badge>
+                                <p>(6)	How important are rent agreements?</p>
+                                <Badge type="primary" :count="52"></Badge>
                             </div>
                         </div>
                     </div>
                     <div class="box-block">
                         <div class="community-category-title p-3">
-                            Categories
+                            Services
                         </div>
                         <div class="community-category-list p-3">
                             <div class=" ccl-item d-flex justify-content-between">
-                                <p>something</p>
-                                <Badge type="primary" :count="10"></Badge>
+                                <p>Asset Management</p>
+                                <Badge type="primary" :count="43"></Badge>
                             </div>
                             <div class=" ccl-item d-flex justify-content-between">
-                                <p>something</p>
-                                <Badge type="primary" :count="10"></Badge>
+                                <p>Facilities Management</p>
+                                <Badge type="primary" :count="65"></Badge>
                             </div>
                             <div class=" ccl-item d-flex justify-content-between">
-                                <p>something</p>
-                                <Badge type="primary" :count="10"></Badge>
+                                <p>International Residential</p>
+                                <Badge type="primary" :count="12"></Badge>
                             </div>
                             <div class=" ccl-item d-flex justify-content-between">
-                                <p>something</p>
-                                <Badge type="primary" :count="10"></Badge>
+                                <p>Research & Consultancy</p>
+                                <Badge type="primary" :count="42"></Badge>
                             </div>
                             <div class=" ccl-item d-flex justify-content-between">
-                                <p>something</p>
-                                <Badge type="primary" :count="10"></Badge>
+                                <p>Residential</p>
+                                <Badge type="primary" :count="51"></Badge>
                             </div>
                             <div class=" ccl-item d-flex justify-content-between">
-                                <p>something</p>
-                                <Badge type="primary" :count="10"></Badge>
+                                <p>Serviced Apartment</p>
+                                <Badge type="primary" :count="24"></Badge>
                             </div>
                         </div>
                     </div>
-                    <div class="box-block">
+                    <!-- <div class="box-block">
                         <div class="community-category-title p-3">
-                            Categories
+                            POPULAR SEARCHES
                         </div>
                         <div class="community-category-list p-3">
                             <div class=" ccl-item d-flex justify-content-between">
-                                <p>something</p>
+                                <p>Pet Friendly Apartments</p>
                                 <Badge type="primary" :count="10"></Badge>
                             </div>
                             <div class=" ccl-item d-flex justify-content-between">
@@ -100,7 +100,7 @@
                                 <Badge type="primary" :count="10"></Badge>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
 
                 </div>
                 <div class="col-12 col-md-8 m-0 p-0">
